f9e520874bedb016c5ab2a9d9900599787647b7d
sejanmiah <sejankhan931@gmail.com>
2025-05-29 20:18:24 +0400

    user application show ui done

--- index.js
+++ index.js
@@ -44,6 +44,25 @@ async function run() {
     });
 
     // Job applications related api
+    app.get('/applications', async(req,res) => {
+        const email = req.query.email;
+
+        const query = {
+            applicant: email
+        };
+        const result = await applicationsCollection.find(query).toArray();
+
+        for(const application of result){
+            const jobId = application.jobId
+            const jobQuery =  {_id: new ObjectId(jobId)}
+            const job = await jobsCollection.findOne(jobQuery)
+            application.company = job.company
+            application.title = job.title
+            application.company_logo = job.company_logo
+        }
+        res.send(result)
+    })
+
     app.post('/applications', async(req,res) => {
         const application = req.body;
         const result = await applicationsCollection.insertOne(application)
